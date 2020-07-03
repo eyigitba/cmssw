59e23fba9f894958fe42c947201b940053d4fd36
Efe Yigitbasi <eyigitba@cern.ch>
2020-07-03 15:10:16 +0200

    Disabling displaced pT scaling LUT for now.

--- L1Trigger/L1TMuonEndCap/src/PtAssignment.cc
+++ L1Trigger/L1TMuonEndCap/src/PtAssignment.cc
@@ -113,7 +113,8 @@ void PtAssignment::process(EMTFTrackCollection& best_tracks) {
 
     pt_assign_engine_dxy_->calculate_pt_dxy(track, feature, prediction);
 
-    pt_dxy = aux_dxy().getTriggerPtDxy(prediction.at(0));
+    // pt_dxy = aux_dxy().getTriggerPtDxy(prediction.at(0));
+    pt_dxy = std::abs(1.0/prediction.at(0));
     dxy = prediction.at(1);
 
     gmt_pt_dxy = aux_dxy().getGMTPtDxy(pt_dxy);
